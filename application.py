1406c42e12cecb8aabae2e8c4066b8a3fa67315b
Matteo Paolucci <matteo.paolucci4@studio.unibo.it>
2021-03-11 22:18:07 +0100

    Modified: application.py
    - application.py: corrected indentation

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import screeninfo
 import keyboard
 import apputils
-import gui
+import frame
 
 
 class Application():
@@ -30,16 +30,16 @@ class Application():
         apputils.readHotkeys(self.hotkeys)
         self.root = parent
         self.root.title('MouseMove')
-        self.frame = gui.guiFrame(self.monitors, self.hotkeys, self, parent)
+        self.frame = frame.guiFrame(self.monitors, self.hotkeys, self, parent)
         self.frame.grid(padx=5, pady=5)
         self.root.withdraw()
         self.frame.createWidgets()
         self.initHotkeys()
 
     def initHotkeys(self):
-    """
-    Set hotkeys for each monitor and to use the application.
-    """
+        """
+        Set hotkeys for each monitor and to use the application.
+        """
 
         for n, monitor in enumerate(self.monitors):
             hotkey = f'{self.hotkeys[n][0]}'
@@ -50,17 +50,17 @@ class Application():
         keyboard.add_hotkey('alt+shift+0', self.root.destroy)
 
     def show(self):
-    """
-    Shows the configuration window.
-    """
+        """
+        Shows the configuration window.
+        """
 
         self.root.deiconify()
         self.root.update()
 
     def restart(self):
-    """
-    Hides the configuration window and re-intialize the hotkeys.
-    """
+        """
+        Hides the configuration window and re-intialize the hotkeys.
+        """
 
         apputils.readHotkeys(self.hotkeys)
         keyboard.unhook_all_hotkeys()
@@ -68,9 +68,9 @@ class Application():
         self.root.withdraw()
 
     def save(self):
-    """
-    Save the hotkeys configuration in a csv file.
-    """
+        """
+        Save the hotkeys configuration in a csv file.
+        """
 
         self.hotkeys[self.frame.prevCombo][0] = self.frame.hotkeyEntry.get()
         apputils.writeHotkeys(self.hotkeys)
